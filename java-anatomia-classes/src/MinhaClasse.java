25788227e14b4abd3f2293f95f911c22c59ddd3e
Yuri Kevin <yurikevin1405@gmail.com>
2023-11-18 19:19:19 -0300

    Testando variáveis

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,19 @@
+public class MinhaClasse {
+    
+  
+
+    public static void main(String[] args) {
+        String nome = "Yuri";
+        String sobrenome = "Kevin";
+
+        String nomeCompleto = (nomeCompleto(nome, sobrenome));
+        System.out.println(nomeCompleto);
+        
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome)
+    {
+        return primeiroNome + " " + segundoNome;
+    }
+
+}
