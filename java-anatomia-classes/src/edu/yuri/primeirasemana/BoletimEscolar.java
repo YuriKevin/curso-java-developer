021749f3803df535a0c5283c63b0415c11874a5e
Yuri Kevin <yurikevin1405@gmail.com>
2023-11-18 19:44:51 -0300

    Entendendo pacotes (packages)

--- java-anatomia-classes/src/edu/yuri/primeirasemana/BoletimEscolar.java
+++ java-anatomia-classes/src/edu/yuri/primeirasemana/BoletimEscolar.java
@@ -1,5 +1,6 @@
-public class BoletimEscolar{
+package edu.yuri.primeirasemana;
 
+public class BoletimEscolar{
     public static void main(String[] args) {
         float numero = 6;
         if (numero<6)
--- java-anatomia-classes/src/edu/yuri/primeirasemana/MinhaClasse.java
+++ java-anatomia-classes/src/edu/yuri/primeirasemana/MinhaClasse.java
@@ -1,14 +1,12 @@
-public class MinhaClasse {
-    
-  
+package edu.yuri.primeirasemana;
 
+public class MinhaClasse {
     public static void main(String[] args) {
         String nome = "Yuri";
         String sobrenome = "Kevin";
 
         String nomeCompleto = (nomeCompleto(nome, sobrenome));
         System.out.println(nomeCompleto);
-        
     }
 
     public static String nomeCompleto (String primeiroNome, String segundoNome)
