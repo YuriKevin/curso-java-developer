69572c75357780a90298c1852867310074b71c41
Yuri Kevin <yurikevin1405@gmail.com>
2023-11-18 19:29:33 -0300

    Trabalhando identação

---
+++ java-anatomia-classes/src/BoletimEscolar.java
@@ -0,0 +1,12 @@
+public class BoletimEscolar{
+
+    public static void main(String[] args) {
+        float numero = 6;
+        if (numero<6)
+            System.out.println("Reprovado");
+        else if(numero==6)
+            System.out.println("Conselho");
+        else
+            System.out.println("Aprovado"); 
+    } 
+}
